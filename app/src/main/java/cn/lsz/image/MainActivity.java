4e409e0b8787211d1ec304f1ba6299324f3fa1bd
LSZ66 <38709941+LSZ66@users.noreply.github.com>
2018-06-27 00:21:26 +0800

    更新了图片选择

--- app/src/main/java/cn/lsz/image/MainActivity.java
+++ app/src/main/java/cn/lsz/image/MainActivity.java
@@ -2,17 +2,23 @@ package cn.lsz.image;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.soundcloud.android.crop.Crop;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     public static Bitmap select;
-    public static String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
-        if (path != null) {
+        if (MainActivity.select != null) {
             ImageView imageView = findViewById(R.id.imageView);
             imageView.setImageBitmap(select);
         } else
@@ -31,25 +37,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnPrimaryColor(View view) {
-        if (path != null)
+        if (select != null)
             startActivity(new Intent(this, PrimaryColor.class));
         else
             Toast.makeText(getApplicationContext(), "请先选择图片", Toast.LENGTH_SHORT).show();
     }
 
     public void btnFilter(View view) {
-        if (path != null)
+        if (select != null)
             startActivity(new Intent(this, PixelsEffect.class));
         else
             Toast.makeText(getApplicationContext(), "请先选择图片", Toast.LENGTH_SHORT).show();
     }
 
     public void btnSelect(View view) {
-        startActivity(new Intent(this, PicList.class));
+        Crop.pickImage(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
+            try {
+                MainActivity.select = MediaStore.Images.Media.getBitmap(getContentResolver(), result.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            Toast.makeText(getApplicationContext(), "请先选择图片", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void saveImage(View view) {
-        if (path != null) {
+        if (select != null) {
             MediaStore.Images.Media.insertImage(getContentResolver(), MainActivity.select, "title", "description");
             Toast.makeText(getApplicationContext(), "保存图片成功", Toast.LENGTH_SHORT).show();
         } else
